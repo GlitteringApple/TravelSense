8f26755aa54e983b153c441b7e3af24f70367ccb
GlitteringApple <69908601+GlitteringApple@users.noreply.github.com>
2026-01-26 22:19:45 +0530

    Added an animated search bar
    Icons are now left-aligned on the top of the screen.
    The previously added search bar is now a compact search icon that expands into a fully fledged search bar when tapped. It reverts back into its icon form when the software keyboard is hidden.
    For now, it is the only functioning icon in the app so far.

--- App.js
+++ App.js
@@ -1,13 +1,21 @@
-import { View, Text, Image, ImageBackground, ScrollView, Button, TextInput, StyleSheet, Pressable,StatusBar, ActivityIndicator, Alert} from 'react-native';
+import { View, Text, Image, ImageBackground, ScrollView, Button, TextInput, StyleSheet, Pressable, StatusBar, ActivityIndicator, Alert, Dimensions, Keyboard } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import ButtonRound from "./components/ButtonRound"
 const mapImg = require("./assets/carte-geographique-du-monde.jpg");
 import Entypo from '@expo/vector-icons/Entypo';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
 import {
   createStaticNavigation,
   useNavigation,
 } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { useEffect, useRef, useState } from 'react';
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  withTiming,
+  interpolate,
+} from 'react-native-reanimated';
 
 const MyTabs = createBottomTabNavigator({
   screens: {
@@ -18,37 +26,158 @@ const MyTabs = createBottomTabNavigator({
 const Navigation = createStaticNavigation(MyTabs);
 
 export default function App() {
-   //Fullscreen component
-  return <View style={{ flex: 1, backgroundColor: "black" }}>
-      <StatusBar translucent backgroundColor="transparent" />
+
+  //0 = fully collapsed, 1 = fully expanded
+  const progress = useSharedValue(0);
+  const maxSearchWidth = useSharedValue(0);
+  const screenWidth = Dimensions.get('window').width;
+  const [isSearchBarFocused, setSearchBarFocused] = useState(true);
+  const inputRef = useRef(null);
+
+  const onLayout = (event) => {
+    maxSearchWidth.value = event.nativeEvent.layout.width;
+  };
+
+  const toggleInput = () => {
+    //If not read-only, focus the text field.
+    if (!isSearchBarFocused) {
+      setTimeout(() => {
+        inputRef.current?.focus();
+      }, 100)
+    }
+    else {
+      inputRef.current?.blur();
+    }
+  };
+
+  useEffect(() => {
+    const keybHidden = Keyboard.addListener('keyboardDidHide', () => toggleSearch());
+
+    return () => {
+      keybHidden.remove();
+    };
+  });
+
+  //Smoothly animates expansion and collapse from 0 to 1.
+  const toggleSearch = () => {
+    setSearchBarFocused(!isSearchBarFocused);
+    if (isSearchBarFocused) {
+      progress.value = withTiming(1, {
+        duration: 300,
+      });
+      setTimeout(() => {
+        inputRef.current?.focus();
+      }, 100)
+    }
+    else {
+      if (!isSearchBarFocused) {
+        inputRef.current?.blur();
+        progress.value = withTiming(0, {
+          duration: 300,
+        });
+      }
+    }
+  };
+
+  //Resizes the input field container.
+  const searchBarStyle = useAnimatedStyle(() => {
+    return {
+      width: interpolate(progress.value, [0, 1], [50, screenWidth - 140]),
+    };
+  });
+
+  //Fades search bar text in, and search icon out.
+  const inputStyle = useAnimatedStyle(() => {
+    return {
+      opacity: interpolate(progress.value, [0, 0.4, 1], [0, 0, 1]),
+    };
+  });
+
+  const iconStyle = useAnimatedStyle(() => {
+    return {
+      opacity: interpolate(progress.value, [0, 0.3], [1, 0]),
+      transform: [
+        {
+          scale: interpolate(progress.value, [0, 1], [1, 0.9]),
+        },
+      ],
+    };
+  });
+
+  //Fullscreen component
+  return <View style={styles.screen}>
+    <StatusBar translucent backgroundColor="transparent" />
     <ImageBackground source={mapImg} style={{ flex: 1 }}>
-      <SafeAreaView>
-      <View style={{ padding: 10, justifyContent: "left", flexDirection: "row", gap: 10}}>
+      <SafeAreaView style={styles.wrapper} onLayout={onLayout}>
+
         <ButtonRound>
           <Entypo name="menu" size={24} color="black" />
         </ButtonRound>
-      <TextInput
-          style={searchBarStyle.input}
-          placeholder="Search TravelSense">
-      </TextInput>
-      <ButtonRound name="location-pin" size={5}>
-        <Entypo name="location-pin" size={24} color="black" />
-      </ButtonRound>
-    </View>
-    </SafeAreaView>
+
+        <Pressable onPress={toggleSearch} style={{ flex: 0 }}>
+          <Animated.View style={[styles.searchContainer, searchBarStyle]}>
+            <Animated.View style={[styles.iconWrapper, iconStyle]}>
+              <FontAwesome name="search" size={20} color="black" />
+            </Animated.View>
+            <Animated.View style={[styles.inputWrapper, inputStyle]}>
+              <TextInput placeholder="Search TravelSense..." style={styles.input} ref={inputRef} readOnly={isSearchBarFocused} />
+            </Animated.View>
+          </Animated.View>
+        </Pressable>
+
+        <ButtonRound name="location-pin" size={5}>
+          <Entypo name="location-pin" size={24} color="black" />
+        </ButtonRound>
+
+      </SafeAreaView>
     </ImageBackground>
   </View>
 }
 
-const searchBarStyle = StyleSheet.create({
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    backgroundColor: "black",
+  },
+  wrapper: {
+    width: "100%",
+    padding: 10,
+    justifyContent: "left",
+    flexDirection: "row",
+    gap: 10,
+  },
   input: {
     flex: 1,
     backgroundColor: "#ffffff",
-    borderRadius: 30,
+    borderRadius: 25,
     height: 50,
     padding: 10,
     marginHorizontal: 0,
     borderWidth: 0,
     elevation: 5,
-  }
-})
+    textAlignVertical: "center",
+  },
+  searchContainer: {
+    height: 50,
+    backgroundColor: "white",
+    justifyContent: "center",
+    paddingHorizontal: 10,
+    borderRadius: 25,
+    overflow: "hidden",
+    elevation: 5,
+  },
+  iconWrapper: {
+    position: "absolute",
+    justifyContent: "center",
+    alignItems: "center",
+    width: 50,
+    height: 50,
+  },
+  inputWrapper: {
+    flex: 1,
+  },
+  input: {
+    color: "gray",
+    fontSize: 16,
+  },
+});
